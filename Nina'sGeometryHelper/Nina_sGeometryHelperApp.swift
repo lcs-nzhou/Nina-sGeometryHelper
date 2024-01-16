16557d256cfee6542832c96f2a09d5a9144fc85f
Nina Zhou <nzhou@lcs.on.ca>
2024-01-16 11:16:43 -0500

    Initial Commit

---
+++ Nina'sGeometryHelper/Nina_sGeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nina_sGeometryHelperApp.swift
+//  Nina'sGeometryHelper
+//
+//  Created by Shuyu Zhou on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Nina_sGeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
